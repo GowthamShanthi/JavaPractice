b1ddd2fe763da50c259475ff71deae4ebdb0047d
Mahalak97 <mahalakshminagaraj123@gmail.com>
2024-04-12 10:02:39 +0530

    Intial commit

---
+++ src/basicStuffs/Datatypes.java
@@ -0,0 +1,27 @@
+package basicStuffs;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//integer datatype	- this can store form -2Billion to +2 billion integer values 	
+int age = 34;
+System.out.println("The age is \n"+age);
+
+//byte dataype - this can store from -127 to +127 - so for small value like age =18 we can use bytes 
+
+byte myage = 33;
+System.out.println("The age is \n"+myage);
+
+//Short datatype - this can store from -32k to +32(32767k)
+short years = 32767;
+System.out.println("The year is\n"+years);
+
+
+		
+		
+	}
+
+}
---
+++ src/basicStuffs/Namachivaya.java
@@ -0,0 +1,17 @@
+package basicStuffs;
+
+/**
+ * @author V GOWTHAM
+ *
+ */
+public class Namachivaya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+System.out.println("Namachivaya Vaalga");
+
+	}
+
+}
